e01bf53ec4f66f7898c4d5a95ace162077da6e73
luoty <tongyi.luo@gmail.com>
2017-11-09 15:52:18 +0800

    添加基本目录

---
+++ README.md
@@ -0,0 +1,11 @@
+
+文件夹用途管理（总目录app）：
+1、base：项目入口、基类
+2、components：项目封装的公共组件
+3、mockData：mock的数据，便于测试使用，同时存放请求接口
+4、resource/...：资源(包括图片、文件、音频、视频等等)
+5、storage：缓存
+6、stores：状态管理
+7、styles：公共样式
+8、utils：工具
+9、pages：项目界面
---
+++ app/pages/test.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class TestPage extends Component {
+    state = {}
+    render() {
+        return (
+            <View style={{ flex: 1 }}>
+                <Text>aaaa</Text>
+            </View>
+        );
+    }
+}
+
+export default TestPage;
