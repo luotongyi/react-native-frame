2850cf7a7168ae58b6fe489468ef5e8aa0a42248
luoty <tongyi.luo@gmail.com>
2017-11-10 15:25:25 +0800

    结构改动

--- app/api/index.js
+++ app/api/index.js
@@ -1,12 +1,12 @@
 
-const BASE_URL = 'http://wap.cq.10086.cn'
+const BASE_URL = 'http://www.baidu.com'
 
 export const VERSION_CHECK_URL = BASE_URL + 'xxxx'
 
 
 
 export default {
-    testArray: [{ url: 'https://www.baidu.com', imageUrl: 'http://wap.cq.10086.cn/mall/ecrm/img/ecom/5041717580596085.jpg' },]
+    testArray: [{ url: 'https://www.baidu.com', name: 'aaa' },]
 
 
 }
